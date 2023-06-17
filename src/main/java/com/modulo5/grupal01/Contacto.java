38b561c80e8e47facb76d0f2ec599423a3dc46fe
EtzelMV <etzelvalderrama@gmail.com>
2023-06-17 14:31:40 -0400

    Actualizacion visual del formulario de contacto

--- src/main/java/com/modulo5/grupal01/Contacto.java
+++ src/main/java/com/modulo5/grupal01/Contacto.java
@@ -1,7 +1,10 @@
 package com.modulo5.grupal01;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -25,26 +28,48 @@ public class Contacto extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	// Obtener el contenido de la plantilla HTML desde un archivo externo
+        InputStream templateStream = getServletContext().getResourceAsStream("templates/plantilla01.html");
+        InputStreamReader templateReader = new InputStreamReader(templateStream, StandardCharsets.UTF_8);
+        
+        // Leer el contenido de la plantilla y guardar en una cadena
+        StringBuilder templateContentBuilder = new StringBuilder();
+        char[] buffer = new char[4096];
+        int charsRead;
+        while ((charsRead = templateReader.read(buffer)) != -1) {
+            templateContentBuilder.append(buffer, 0, charsRead);
+        }
+        String templateContent = templateContentBuilder.toString();
+        
+        // Generar el contenido específico de la página
+        String pageContent = "<h1>Contacto</h1>"
+        					+ "<div class=\"container\">"
+        						+ "<form class=\"custom-form\" method=\"post\" action=\"Enviado\">"
+        							+ "<div class=\"mb-3\">"
+        								+ "<label for=\"name\" class=\"form-label\">Nombre:</label>"
+        								+ "<input type=\"text\" class=\"form-control\" id=\"name\">"
+        							+ "</div>"
+        							+ "<div class=\"mb-3\">"
+        								+ "<label for=\"email\" class=\"form-label\">Email:</label>"
+        								+ "<input type=\"email\" class=\"form-control\" id=\"email\">"
+        							+ "</div>"
+        							+ "<div class=\"mb-3\">"
+        								+ "<textarea class=\"form-control\" id=\"message\" rows=\"5\" placeholder=\"Ingrese el motivo de su consulta aquí...\">"
+        								+ "</textarea>"
+        							+ "</div>"
+        							+ "<button type=\"submit\" class=\"btn btn-primary\">Enviar</button>"
+        						+ "</form>"
+        					+ "</div>";
+        
+        // Reemplazar la etiqueta <main> en la plantilla con el contenido específico de la página
+        String finalContent = templateContent.replace("<main></main>", "<main>" + pageContent + "</main>");
+        
+        // Establecer el tipo de contenido de la respuesta
         response.setContentType("text/html");
         
-        // Escribir el contenido HTML del formulario de contacto
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Grupal 01</title>");
-        out.println("</head>");
-        out.println("<body style=\"background-color:DarkBlue; color:White; text-align:center;\">");
-        out.println("<h1>Contacto</h1>");
-        out.println("<form method=\"post\" action=\"Enviado\">");
-        out.println("<label for=\"name\">Nombre:</label>");
-        out.println("<input type=\"text\" name=\"name\" id=\"name\"><br><br>");
-        out.println("<label for=\"email\">Email:</label>");
-        out.println("<input type=\"email\" name=\"email\" id=\"email\"><br><br>");
-        out.println("<p>Aqui voy a colocar un cuadro de texto.</p>");
-        out.println("<input type=\"submit\" value=\"Enviar\">");
-        out.println("</form>");
-        out.println("</body>");
-        out.println("</html>");
+        // Escribir el contenido en la respuesta del servlet
+        response.getWriter().write(finalContent);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
